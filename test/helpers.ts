cd10954d5ddc3d346b50f791754819b9fdd836d5
Sandy <70289658+sndyx@users.noreply.github.com>
2025-04-13 11:26:07 -0400

    Update tests to ignore carriage returns

--- test/helpers.ts
+++ test/helpers.ts
@@ -11,7 +11,7 @@ export function readCases(path: string): { name: string, source: string }[] {
     for (const file of files) {
         if (!file.endsWith(".htsl")) continue;
         const name = file.substring(0, file.length - 5).replaceAll("_", " ");
-        const source = readFileSync(path + `/${file}`).toString();
+        const source = readFileSync(path + `/${file}`).toString().replaceAll("\r", "");
         entries.push({ name, source });
     }
 
